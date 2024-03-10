0834e22b8751384f25925ffba0c2aa563aef247a
lx522 <104619116+lx522@users.noreply.github.com>
2024-03-10 15:42:49 +0800

    Update GCH.py

--- GCH/GCH.py
+++ GCH/GCH.py
@@ -71,18 +71,9 @@ class BGRL(torch.nn.Module):
         # forward target networky
         with torch.no_grad():
             target_y = self.target_encoder(target_x).detach()
-        knn = posgraph(online_x.edge_index,online_y,target_y,k)# online_x.edge_index(2,152037) online_y(7650,128) target_y(7650,128) k=2 --> knn(2,15300)
+        knn = posgraph(online_x.edge_index,online_y,target_y,k)
 
         return online_y, target_y, knn# (7650,128) (7650,125) (2,15300)
-        # ------------------------------------------------------------COSTA
-        # # forward online network
-        # online_y = self.online_encoder(online_x)
-        #
-        # with torch.no_grad():
-        #     target_y = self.target_encoder(target_x).detach()
-        #
-        # return online_y, target_y
-        #_______________________________________________________________
 
         ret = ret.mean() if mean else ret.sum()
 
@@ -111,7 +102,7 @@ class BGRL(torch.nn.Module):
         uniform = self.gamma * (self.uniformity(user_e) + self.uniformity(item_e)) / 2
 
         return align + uniform
-    def loss(self, z1: torch.Tensor, z2: torch.Tensor, mean: bool = True):# z1=q1(7650,128),z2=y2(7650,128)
+    def loss(self, z1: torch.Tensor, z2: torch.Tensor, mean: bool = True):
 
         l1 = self.semi_loss(z1, z2)# Loss_cl(7650,)
         # l2 = self.semi_loss(z2, z2)
@@ -129,12 +120,7 @@ class BGRL(torch.nn.Module):
 
         return 2 - 2 * (x * y).sum(dim=-1)#(15300,)
 
-    # def semi_loss(self, z1: torch.Tensor, z2: torch.Tensor):
-    #     f = lambda x: torch.exp(x / self.tau)
-    #     refl_sim = f(self.sim(z1, z2))# 相似性矩阵(7650,7650)
-    #     # refl_sim.diag() 表示取 refl_sim 矩阵的对角线元素，这些对角线元素表示每个样本与自己的相似性
-    #     loss_semi = -torch.log(refl_sim.diag() / (refl_sim.sum(1) - refl_sim.diag()))
-    #     return loss_semi#  =8.9657
+    
     def semi_loss(self, z1: torch.Tensor, z2: torch.Tensor):
         f = lambda x: torch.exp(x / self.tau)
         refl_sim = f(self.sim(z1, z1))# (7650,7650)
@@ -146,8 +132,7 @@ class BGRL(torch.nn.Module):
     def sim(self, z1: torch.Tensor, z2: torch.Tensor):
         z1 = F.normalize(z1)
         z2 = F.normalize(z2)
-        return torch.mm(z1, z2.t())# torch.mm()=(7650,7650) 表示矩阵相乘，计算相似性分数，z2.t() 表示将 z2 进行转置操作
-
+        return torch.mm(z1, z2.t())
 
 def load_trained_encoder(encoder, ckpt_path, device):
     r"""Utility for loading the trained encoder."""
@@ -180,15 +165,12 @@ def posgraph(adj, student, teacher, top_k):# adj=（2，152037）
     n_data, d = student.shape# 7650 128
     student = F.normalize(student, dim=-1, p=2)# 行归一化
     teacher = F.normalize(teacher, dim=-1, p=2)
-    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach())# similarity=（7650，7650）
-    # similarity += torch.eye(n_data, device=device) * 10
-    # 值_=(7650,2) 维度I_knn=(7650,2)
-    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)# k=2
-    tmp = torch.LongTensor(np.arange(n_data)).unsqueeze(-1).to(device)# tmp=(7650,1)
-    # adj = torch.sparse.FloatTensor(adj, torch.ones_like(adj[0]), [student.shape[0], student.shape[0]])
-    # knn_neighbor=(7650,7650)
-    knn_neighbor = create_sparse(I_knn)# knn_neighbor(7650,7650) 共15300=7650*2个值
-    # knn_neighbor = abs(knn_neighbor - adj)
+    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach())
+  )
+    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)
+    tmp = torch.LongTensor(np.arange(n_data)).unsqueeze(-1).to(device)
+ 
+    knn_neighbor = create_sparse(I_knn)
 
     return knn_neighbor._indices()
     # return knn_neighbor
@@ -203,39 +185,27 @@ def scipy_sparse_mat_to_torch_sparse_tensor(sparse_mx):
 
 def knngraph(data, top_k):
 
-    # x = LocalDegreeProfile()(data.clone())
-    # similarity1 = torch.matmul(x, torch.transpose(x, 1, 0).detach())
-    # _, I_knn1 = similarity1.topk(k=5, dim=1, largest=True, sorted=True)
-    # s_neighbor = create_sparse(I_knn1).to(device)
     student = F.normalize(data.x, dim=-1, p=2)# student（7650，745）行归一化
     teacher = F.normalize(data.x, dim=-1, p=2)# teacher（7650，745）行归一化
-    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach())# 矩阵相乘similarity（7650，7650）
-    # similarity += torch.eye(n_data, device=device) * 10
+    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach())
 
-    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)# dim=1 I_knn（7650，4）每行取前四名 _=（7650，4）
+    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)
 
-    knn_neighbor = create_sparse(I_knn.to(device))# knn_neighbor=tensor(indices=tensor([[   0,    0,    0,  ..., 7649, 7649, 7649][   0, 1561, 2497,  ..., 5836, 3498, 6467]]),values=tensor([1, 1, 1,  ..., 1, 1, 1]),device='cuda:0', size=(7650, 7650), nnz=30600, layout=torch.sparse_coo)
-    # adj = to_scipy_sparse_matrix(adj)
-    # adj = sparse_mx_to_torch_sparse_tensor(adj).to(device)
+    knn_neighbor = create_sparse(I_knn.to(device))
 
     return knn_neighbor._indices()
 
 
 def ind(z1,z2, top_k):
-    # x = LocalDegreeProfile()(data.clone())
-    # similarity1 = torch.matmul(x, torch.transpose(x, 1, 0).detach())
-    # _, I_knn1 = similarity1.topk(k=5, dim=1, largest=True, sorted=True)
-    # s_neighbor = create_sparse(I_knn1).to(device)
+   
     student = F.normalize(z1, dim=-1, p=2)  # student（7650，745）行归一化
     teacher = F.normalize(z2, dim=-1, p=2)  # teacher（7650，745）行归一化
-    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach())  # 矩阵相乘similarity（7650，7650）
-    # similarity += torch.eye(n_data, device=device) * 10
-
-    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)  # dim=1 I_knn（7650，4）每行取前四名 _=（7650，4）
+    similarity = torch.matmul(student, torch.transpose(teacher, 1, 0).detach()) 
+   
+    _, I_knn = similarity.topk(k=top_k, dim=1, largest=True, sorted=True)  
 
     knn_neighbor = create_sparse(I_knn.to(
-        device))  # knn_neighbor=tensor(indices=tensor([[   0,    0,    0,  ..., 7649, 7649, 7649][   0, 1561, 2497,  ..., 5836, 3498, 6467]]),values=tensor([1, 1, 1,  ..., 1, 1, 1]),device='cuda:0', size=(7650, 7650), nnz=30600, layout=torch.sparse_coo)
-
+        device))  
 
     return knn_neighbor._indices()
 def create_sparse(I):
